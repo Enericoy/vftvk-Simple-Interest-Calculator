6cd85edd174679c1655465385e0033e88a35a573
Enericoy <70982957+Enericoy@users.noreply.github.com>
2021-12-15 20:56:46 +0000

    Removed duplicated JS
    Removed extra functions.

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-function compute2()
-{
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-
-    var interest = principal * years * rate /100;
-
-    var year = new Date().getFullYear()+parseInt(years);
-
-    document.getElementById("result").innerHTML=principal;
-
-    // document.getElementById("result").innerHTML="If you deposit ";
-
-
-}
-
 function compute(){
     document.getElementById("result").innerHTML = "Hello World";
   }
