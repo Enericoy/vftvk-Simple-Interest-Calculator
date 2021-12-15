566fcc2ad963898bd93111f07d2e6866eced52f3
Enericoy <70982957+Enericoy@users.noreply.github.com>
2021-12-15 20:55:13 +0000

    Updated Javascript
    Added JS functionality

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function compute()
+function compute2()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate /100;
+
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML=principal;
+
+    // document.getElementById("result").innerHTML="If you deposit ";
+
+
+}
+
+function compute(){
+    document.getElementById("result").innerHTML = "Hello World";
+  }
+
+function updateRate() 
+{
+    var rateval = document.getElementById("interest").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
         
