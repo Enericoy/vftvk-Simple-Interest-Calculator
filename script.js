a29a1929d6365f61391756a3eb3052fcf9ceb36e
Enericoy <70982957+Enericoy@users.noreply.github.com>
2021-12-15 22:18:02 +0000

    Final submission
    Added comments and finished the code.

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 function compute(){
+
+    // Again check the amount if positive
+    if (check_principal()){
+
     p = document.getElementById("principal").value;
 
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("interest").value;
     var years = document.getElementById("years").value;
 
+    // Caculate interest:
     var interest = principal * years * rate /100;
 
+    // Calculate year:
     var year = new Date().getFullYear()+parseInt(years);
 
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
   }
+}
 
+// Change percentage when the slider is moved:
 function updateRate() 
 {
     var rateval = document.getElementById("interest").value;
     document.getElementById("rate_val").innerText=rateval+"%";
+}
+
+// Check for positive amount
+function check_principal(){
+    var value = document.getElementById('principal').value;
+            if ( value <= 0) {
+                 alert("Enter a positive number!");
+                 document.getElementById("principal").focus();
+                 return false;
+            } else {
+                return true;
+            }
+
 }
         
