ff4720e3b1aeb9472843083169fc23cb1e209bc7
Enericoy <70982957+Enericoy@users.noreply.github.com>
2022-01-11 14:58:47 +0000

    Changed to Share Price Calculator
    Changed App.

--- script.js
+++ script.js
@@ -5,25 +5,18 @@ function compute(){
 
     p = document.getElementById("principal").value;
 
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("interest").value;
-    var years = document.getElementById("years").value;
+    var price = document.getElementById("principal").value;
+    var value_gbp = ((price/100)*7262);
+    var bought_gbp = 4041.71;
 
-    // Caculate interest:
-    var interest = principal * years * rate /100;
+    // Caculate P_L:
+    var P_L = value_gbp-bought_gbp;
+    var percentage_PL = ((value_gbp-bought_gbp)/bought_gbp)*100;
 
-    // Calculate year:
-    var year = new Date().getFullYear()+parseInt(years);
-
-    document.getElementById("result").innerHTML="If you deposit <mark>"+ principal +" </mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>";
+    document.getElementById("result").innerHTML="Previous Value : £" +
+     bought_gbp.toFixed(2) + "\<br\>Current Value : £" + value_gbp.toFixed(2) + "\<br\>Current P/L : <mark> £"+
+      P_L.toFixed(2) + " (" + percentage_PL.toFixed(2) + "%)" + " </mark> \<br\>Current breakeven point is 56p";
   }
-}
-
-// Change percentage when the slider is moved:
-function updateRate() 
-{
-    var rateval = document.getElementById("interest").value;
-    document.getElementById("rate_val").innerText=rateval+"%";
 }
 
 // Check for positive amount
